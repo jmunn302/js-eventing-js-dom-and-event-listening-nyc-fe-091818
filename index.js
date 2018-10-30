b8ad6eabef974d3b1c91b6eaecdb0aaaa03b494e
Julie Munn <jmunn302@gmail.com>
2018-10-30 22:46:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const divs = document.querySelectorAll('div')
 console.log(divs)
 
 function onDivClick(event) {
-  console.log(this.firstChild.nodeValue.trim())
+console.log(this.firstChild.nodeValue.trim() + ' bubbled')
 }
 
 for (let div of divs) {
